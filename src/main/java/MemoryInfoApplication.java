10c6fafa6ecbbfb180b4ee1f35497bed05bfb6a4
Dietmar Darimont <dietmar.darimont@gmail.com>
2024-01-07 16:42:40 +0100

    Initial commit

--- src/main/java/MemoryInfoApplication.java
+++ src/main/java/MemoryInfoApplication.java
@@ -1,25 +1,24 @@
 public class MemoryInfoApplication {
 
     public static void main(String[] args) {
-        MemoryInfoApplication app = new MemoryInfoApplication();
-        app.logMemoryInfo();
+        MemoryInfoApplication.loggeSpeicherInfo();
     }
 
-    public void logMemoryInfo() {
+    public static void loggeSpeicherInfo() {
         final Runtime runtime = Runtime.getRuntime();
 
-        final long maxMemory = runtime.maxMemory();
-        final long allocatedMemory = runtime.totalMemory();
-        final long freeMemory = runtime.freeMemory();
-        final long totalFreeMemory = freeMemory + maxMemory - allocatedMemory;
+        final long maxSpeicher = runtime.maxMemory();
+        final long zugewiesenerSpeicher = runtime.totalMemory();
+        final long freierSpeicher = runtime.freeMemory();
+        final long gesamtFreierSpeicher = freierSpeicher + maxSpeicher - zugewiesenerSpeicher;
 
-        System.out.println("free memory: " + toMB(freeMemory) + "MB");
-        System.out.println("allocated memory: " + toMB(allocatedMemory) + "MB");
-        System.out.println("max memory: " + toMB(maxMemory) + "MB");
-        System.out.println("total free memory: " + toMB(totalFreeMemory) + "MB");
+        System.out.println("Freier Speicher: " + inMB(freierSpeicher) + "MB");
+        System.out.println("Zugewiesener Speicher: " + inMB(zugewiesenerSpeicher) + "MB");
+        System.out.println("Maximaler Speicher: " + inMB(maxSpeicher) + "MB");
+        System.out.println("Gesamter freier Speicher: " + inMB(gesamtFreierSpeicher) + "MB");
     }
 
-    private long toMB(long b) {
-        return b / (1024 * 1024);
+    private static long inMB(long byteWert) {
+        return byteWert / (1024 * 1024);
     }
 }
